5cc5d3f1eaa5262c9a81b2172a61ea1f1c20b0e2
Shreyash <shreyashghuge@gmail.com>
2025-12-14 18:30:46 +0530

    changes added

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,11 +1,11 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsItem from './NewsItem'
 import Loadingpic from './Loadingpic';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import PropTypes from 'prop-types'
 
-const News=(props)=> {
-    
+const News = (props) => {
+
     const [articles, setArticles] = useState([]);
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
@@ -14,7 +14,7 @@ const News=(props)=> {
     const captial = (word) => {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
-    const update =async()=> {
+    const update = async () => {
         props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         props.setProgress(30);
@@ -26,48 +26,49 @@ const News=(props)=> {
         setLoading(false)
         settotalResults(allData.totalResults)
         props.setProgress(100);
+        console.log(url)
     }
     useEffect(() => {
         update();
         // eslint-disable-next-line
     }, []);
 
-    const fetchData = async() => {
-        
-        
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;       
-        setPage(page+1)
-        
+    const fetchData = async () => {
+
+
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+        setPage(page + 1)
+
         let data = await fetch(url);
         let allData = await data.json();
         setArticles(articles.concat(allData.articles))
-        
+
         settotalResults(allData.totalResults)
     }
-        return (
-            <>
-                <div className='container my-4'>
-                    <h2 className='my-50 text-center' style={{ margin: '35px 0px', marginTop: '90px' }} >DailyNews.com - Top {captial(props.category)} Headlines</h2>
-                    {loading && <Loadingpic />}
-                    <InfiniteScroll
-                        dataLength={articles.length}
-                        next={fetchData}
-                        hasMore={articles.length !== totalResults}
-                        loader={<Loadingpic />}
-                    >
-                        <div className="row">
-                            {articles.map((element) => {
-                                return <div className="col-md-3" key={element.url}>
-                                    <NewsItem title={element.title ? element.title : " "} description={element.description ? element.description : " "} imgUrl={element.urlToImage} url={element.url} date={element.publishedAt} author={element.author ? element.author : "Unknown"} name={element.source.name} />
-                                </div>
-                            })}
+    return (
+        <>
+            <div className='container my-4'>
+                <h2 className='my-50 text-center' style={{ margin: '35px 0px', marginTop: '90px' }} >DailyNews.com - Top {captial(props.category)} Headlines</h2>
+                {loading && <Loadingpic />}
+                <InfiniteScroll
+                    dataLength={articles.length}
+                    next={fetchData}
+                    hasMore={articles.length !== totalResults}
+                    loader={<Loadingpic />}
+                >
+                    <div className="row">
+                        {articles.map((element) => {
+                            return <div className="col-md-3" key={element.url}>
+                                <NewsItem title={element.title ? element.title : " "} description={element.description ? element.description : " "} imgUrl={element.urlToImage} url={element.url} date={element.publishedAt} author={element.author ? element.author : "Unknown"} name={element.source.name} />
+                            </div>
+                        })}
 
-                        </div>
-                    </InfiniteScroll>
-                </div>
-            </>
-        )
-    
+                    </div>
+                </InfiniteScroll>
+            </div>
+        </>
+    )
+
 }
 
 News.defaultProps = {
